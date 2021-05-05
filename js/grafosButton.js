2339b9f0ef4f66c7b6489f6fa5363d987a104c9e
Renata L. R. de Sena <renatalrdesena@gmail.com>
2021-05-05 01:56:11 -0300

    Testes Adicionar Aresta e Vértice

--- js/grafosButton.js
+++ js/grafosButton.js
@@ -1,20 +1,48 @@
 function adicionaGrafo(){
     grafo =  new Grafo(true,true);
-    if (grafo) {
+    if (grafo.existeGrafo()) {
         imprimeNotificacao("Grafo criado com sucesso!", "success");
+        console.log("criei");
     } else {
         imprimeNotificacao("Erro ao criar grafo!", "error");
     }
 }
 
-function adicionaVertice(){  
-    var vertice = document.getElementById('inputAddVertice');
-    if(vertice.value != ''){
-        grafo.addVertice(vertice.value);
-        imprimeNotificacao("Vertice adicionado com sucesso", "success");
-        vertice.value = '';
-    }else{
-        imprimeNotificacao("Valores não podem ser vazios!", "error");
+function adicionaVertice(){ 
+    if (grafo.existeGrafo()) {
+        console.log("existe grafo");
+        var vertice = document.getElementById('inputAddVertice');
+        if(vertice.value != ''){
+            grafo.addVertice(vertice.value);
+            imprimeNotificacao("Vertice adicionado com sucesso", "success");
+            vertice.value = '';
+        }else{
+            imprimeNotificacao("Valores não podem ser vazios!", "error");
+        }
+    } else {
+        console.log("nao existe grafo");
+        imprimeNotificacao("É preciso criar um grafo antes de adicionar um vértice", "error");
+    }
+}
+
+function adicionaAresta(){
+    if (grafo.existeGrafo()) {
+        var vertice1 = document.getElementById('inputAddArestaPond1');
+        var vertice2 = document.getElementById('inputAddArestaPond2');
+        var peso = document.getElementById('inputAddPesoPond');
+        if (!grafo.existeVertice(vertice1) || !grafo.existeVertice(vertice2)) {
+            imprimeNotificacao("É preciso criar os vértices antes de adicionar uma aresta", "error");
+        }
+        if (vertice1.value != '' && vertice2.value != '' && peso.value != ''){
+            grafo.addAresta(vertice1.value, vertice2.value, peso.value);
+            vertice1.value = '';
+            vertice2.value = '';
+            peso.value = '';
+        }else{
+            imprimeNotificacao("Valores não podem ser vazios!", "error");
+        }
+    } else {
+        imprimeNotificacao("É preciso criar um grafo antes de adicionar uma aresta", "error");
     }
 }
 
--- js/grafosLib.js
+++ js/grafosLib.js
@@ -5,7 +5,47 @@ function Grafo(_direcionado,_ponderado){
     this.ponderado = _ponderado;
 }
 
+Grafo.prototype.existeGrafo = function (){
+    if (this.vertices && this.ligacao && this.direcionado && this.ponderado) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
 Grafo.prototype.addVertice = function (vertice){
     this.vertices.push(vertice);
     this.ligacao[vertice] = [];
+};
+
+Grafo.prototype.addAresta = function (vertice1,vertice2,_peso){
+    if (this.direcionado) {
+        console.log(this.ligacao[vertice1].push([vertice2, _peso]));  
+        imprimeNotificacao("Aresta adicionada com sucesso!", "success");
+    } else {
+        imprimeNotificacao("Erro ao adicionar aresta, verifique se os vértices foram inseridos corretamente!", "error");
+    }
+};
+
+Grafo.prototype.existeVertice = function (vertice) {
+    //var logger = document.getElementById('log');
+
+    for(var i=0; i<this.vertices.length; i++){
+        if(this.vertices[i] === vertice){
+            console.log(this.vertices[i]);
+            console.log('Existe vértice');
+            
+            imprimeNotificacao("Existe Vértice! Verifique o console", "success");
+            
+            //logger.innerHTML += this.vertices[i] + '<br />';  
+
+            return true;
+        }
+    }
+    console.log('Vértice não encontrado');
+    
+    imprimeNotificacao("Vértice não encontrado!", "warn");
+
+    //logger.innerHTML = '<br />';  
+    return false;
 };
