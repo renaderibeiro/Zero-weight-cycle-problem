088d3b4be965fd8f43c67fa3b2ab9b8b3ed5b630
Renata L. R. de Sena <renatalrdesena@gmail.com>
2021-05-29 14:12:14 -0300

    Acréscimos de funções e alterações nas notificações

--- js/grafosButton.js
+++ js/grafosButton.js
@@ -1,3 +1,8 @@
+/*########################################################################################################        RETORNAR LIGAÇÕES - Samuel Brati Favarin
+
+            FUNÇÕES DE INSERÇÃO
+#######################################################################################################*/
+
 contador = 0;
 
 function adicionaGrafo(){
@@ -17,22 +22,124 @@ function adicionaGrafo(){
 
 function adicionaVertice(){
     var vertice = document.getElementById('inputAddVertice');
-    if (contador == 1) {
-        if (!grafo.existeVertice(vertice.value)) { // se não existe vértice pode adicionar
-            if(vertice.value != ''){
-                grafo.addVertice(vertice.value);
+    if (!grafo.existeVertice(vertice.value) && true) { // se não existe vértice pode adicionar
+        if(vertice.value != ''){
+            if (grafo.addVertice(vertice.value)) {
                 imprimeNotificacao("Vertice adicionado com sucesso", "success");
-                vertice.value = '';
-            }else{
-                imprimeNotificacao("Valores não podem ser vazios", "error");
+            } else {
+                imprimeNotificacao("Erro ao adicionar vértice", "error");
             }
-        } else {
-            imprimeNotificacao("O vértice já existe", "error");
+            vertice.value = '';
         }
-    } else {
-        imprimeNotificacao("É necessário criar um grafo para adicionar o vértice", "error");
     }
 }
+
+function adicionaArcoPonderado(){
+    var vertice1 = document.getElementById('inputAddArestaPond1');
+    var vertice2 = document.getElementById('inputAddArestaPond2');
+    var peso = document.getElementById('inputAddPesoPond');
+    if(vertice1.value != '' && vertice2.value != '' && peso.value != ''){
+        if (grafo.existeVertice(vertice1.value) && grafo.existeVertice(vertice2.value) && true) {
+            if (!grafo.existeLigacao(vertice1.value, vertice2.value) && true){ // verifica se já existe ligacao e se n existir adidiona
+                if (grafo.addArcoPonderado(vertice1.value, vertice2.value, peso.value)) {
+                    imprimeNotificacao("Aresta adicionada com sucesso", "success");
+                } else {
+                    imprimeNotificacao("Erro ao adicionar aresta", "error");
+                }
+                vertice1.value = '';
+                vertice2.value = '';
+                peso.value = '';
+            }
+        }
+    }
+}
+
+/*########################################################################################################        RETORNAR LIGAÇÕES - Samuel Brati Favarin
+
+            FUNÇÕES DE REMOÇÃO
+#######################################################################################################*/
+
+
+function removeVertice(){
+    var vertice = document.getElementById('inputDelVertice');
+    if (grafo.existeVertice(vertice.value) && true) {
+        if(vertice.value != ''){ 
+            if (grafo.removerVertice(vertice.value)) {
+                imprimeNotificacao("Vértice removido com sucesso", "success");
+            } else {
+                imprimeNotificacao("Erro ao remover vértice", "error");
+            }
+            vertice.value = '';
+        }
+    } else {
+        imprimeNotificacao("Vértice não existe!", "warn");
+    }
+}
+
+function removeLigacao(){
+    var origem = document.getElementById('inputDelLigacao1');
+    var destino = document.getElementById('inputDelLigacao2');
+    if(origem.value != '' && destino.value){
+        if (grafo.existeVertice(origem.value) && grafo.existeVertice(destino.value) && true) {
+            if (grafo.existeLigacao(origem.value, destino.value) && true){ // verifica se já existe ligacao e se n existir adidiona
+                if (grafo.removerLigacao(origem.value, destino.value)){
+                    imprimeNotificacao("Aresta removida com sucesso", "success");
+                } else {
+                    imprimeNotificacao("Erro ao remover aresta", "error");
+                }
+                origem.value = '';
+                destino.value = ''; 
+            }
+        }
+    }
+}
+
+/*########################################################################################################        RETORNAR LIGAÇÕES - Samuel Brati Favarin
+
+            FUNÇÕES DE OPERAÇÕES
+#######################################################################################################*/
+
+function retornaLigacoes(){
+    limparConsole()
+    var vertice = document.getElementById('inputRetornarLigacoes');
+    if(vertice.value != ''){
+        grafo.retornarLigacoes(vertice.value);
+        imprimeNotificacao("Ligações retornadas! Verifique o console", "success");
+        vertice.value = '';
+    }
+}
+
+function existeLigacao(){
+    var origem = document.getElementById('inputExisteLigacaoOrigem');
+    var destino = document.getElementById('inputExisteLigacaoDestino');
+    if(origem.value != '' && destino.value != ''){  
+        if (grafo.existeLigacao(origem.value, destino.value) && true){
+            imprimeNotificacao("Existe aresta", "success");  
+            origem.value = '';
+            destino.value = '';
+        } else {
+            imprimeNotificacao("Não existe aresta", "error");
+        }
+    }
+}
+
+function existeVertice(){
+    var vertice = document.getElementById('inputExisteVertice');
+    if(vertice.value != ''){
+        if (grafo.existeVertice(vertice.value)) {
+            imprimeNotificacao("Existe vértice", "success");
+            vertice.value = '';
+        } else {
+            imprimeNotificacao("Não existe vértice", "error");
+        }
+    }
+}
+
+function limparConsole(){
+    var logger = document.getElementById('log');  
+    logger.innerHTML = '<br />';
+}
+
 
 
 
--- js/grafosLib.js
+++ js/grafosLib.js
@@ -1,9 +1,60 @@
+//funcoes de acao e criacao
+
 function Grafo(_direcionado,_ponderado){
     this.vertices = [];
     this.ligacao  = [];
     this.direcionado = _direcionado;
     this.ponderado = _ponderado;
 }
+
+Grafo.prototype.addVertice = function (vertice){
+    this.vertices.push(vertice);
+    this.ligacao[vertice] = [];
+    return true;
+};
+
+Grafo.prototype.addArcoPonderado = function (vertice1,vertice2,_peso){
+    if (this.direcionado) {
+        this.ligacao[vertice1].push([vertice2, _peso]);  
+        //imprimeNotificacao("Aresta adicionada com sucesso!", "success");
+        console.log('aresta adicionada');
+        return true;
+    }
+};
+
+Grafo.prototype.removerVertice = function (vertice) {
+    var index_vertice = this.vertices.indexOf(vertice);
+    if (index_vertice == -1){
+        console.log("Vértice não existe");
+        //imprimeNotificacao("Vértice não existe!", "warn");
+        return null;
+    }
+
+    this.vertices.splice(index_vertice, 1);
+    for(var pos=0; pos < this.vertices.length; pos++) {
+        this.removerLigacao(this.vertices[pos], vertice);
+    }
+    delete this.ligacao[vertice];
+    //imprimeNotificacao("Vértice removido com sucesso!", "success");
+    return true;
+};
+
+Grafo.prototype.removerLigacao = function (origem,destino) {
+    if(this.direcionado){
+        for(var i =0; i<this.ligacao[origem].length; i++ ) {
+            if (this.ligacao[origem][i][0] === destino) {
+                //DELETA PESO
+                this.ligacao[origem][i].pop();
+                //DELETA VERTICE
+                this.ligacao[origem][i].pop();
+                this.ligacao[origem].splice(i, 1);
+                return true;
+            }
+        }
+    }
+};
+
+// funções de checagem
 
 Grafo.prototype.existeGrafo = function (){
     if (this.direcionado === true && this.ponderado === true) {
@@ -13,23 +64,23 @@ Grafo.prototype.existeGrafo = function (){
     }
 };
 
-Grafo.prototype.addVertice = function (vertice){
-    this.vertices.push(vertice);
-    this.ligacao[vertice] = [];
-    return true;
-};
-
-Grafo.prototype.addAresta = function (vertice1,vertice2,_peso){
-    if (this.direcionado) {
-        console.log(this.ligacao[vertice1].push([vertice2, _peso]));  
-        imprimeNotificacao("Aresta adicionada com sucesso!", "success");
-    } else {
-        imprimeNotificacao("Erro ao adicionar aresta, verifique se os vértices foram inseridos corretamente!", "error");
+Grafo.prototype.existeLigacao = function (origem,destino) {
+    if(this.direcionado){
+        for(var i =0; i<this.ligacao[origem].length; i++ ) {
+            if (this.ligacao[origem][i][0] === destino) {
+                console.log('Existe aresta');
+                //imprimeNotificacao("Existe aresta", "success");
+                return true;
+            }
+        }
+        console.log('Aresta não encontrada');
+        //imprimeNotificacao("Aresta não encontrada", "error");
+        return false;
     }
 };
 
 Grafo.prototype.existeVertice = function (vertice) {
-    //var logger = document.getElementById('log');
+    var logger = document.getElementById('log');
     for(var i=0; i<this.vertices.length; i++){
         if(this.vertices[i] === vertice){
             console.log(this.vertices[i]);
@@ -38,7 +89,75 @@ Grafo.prototype.existeVertice = function (vertice) {
             return true;
         } 
     }
-    console.log('Nao Existe vértice');
+    console.log('Não existe vértice');
     //logger.innerHTML = '<br />';  
     return false;
+};
+
+Grafo.prototype.retornarLigacoes = function (vertice) {
+    var logger = document.getElementById('log');  
+    for(var i=0;i < this.ligacao[vertice].length; i++) {
+        logger.innerHTML += ' [ ' + this.ligacao[vertice][i][0] + ' ] ';     
+    }
+    return this.ligacao[vertice];
+};
+
+// IMPRESSÃO GRAFO EM CANVA
+Grafo.prototype.desenhaCanvasLigacoes = function (tipo){
+
+    //Config do canvas
+    canvas  = document.getElementById('myCanvas');
+    ctx     = canvas.getContext('2d');
+    canvas.width  = 1330;
+    canvas.height = 650;
+
+    //Variáveis do vértice
+    var vertices = [];
+    var auxVertices = [];
+
+    //Variaveis das ligações - Atualizado por Vinícius A.M 30/10/2017
+    var ligacoes = this.iniciaControle(this.ligacao, this.vertices);
+    var auxLigacoes = [];
+
+    //Percorre todos os vertice - Atualizado por Vinícius A.M 30/10/2017
+    for (i = 0; i < this.vertices.length; i++){
+
+        //Insere no vertices um array com a vertice + cor
+        auxVertices = new Array();
+        auxVertices[0] = this.vertices[i];
+        auxVertices[1] = 'red';
+
+        vertices.push(auxVertices);
+    }
+
+    console.log(vertices);
+    console.log(ligacoes);
+
+    if(tipo == "welshAndPowell"){
+        start(canvas, this.welshAndPowell(), ligacoes, grafo);
+    }else if(tipo == "dsatur"){
+        start(canvas, this.dsatur(), ligacoes, grafo);
+    }else if(tipo == "kruskal"){
+        start(canvas, this.welshAndPowell(), this.kruskal(), grafo);
+    }else if(tipo == "prim"){
+        start(canvas, this.dsatur(), this.prim(), grafo);
+    }
+};
+
+Grafo.prototype.retornaCssColors = function (){
+    var css = ["PaleGreen","Magenta","Aqua","Moccasin","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet",
+        "Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan",
+        "DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon",
+        "DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey",
+        "DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow",
+        "HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral",
+        "LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray",
+        "LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+        "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose",
+        "NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleTurquoise",
+        "PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon",
+        "SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan",
+        "Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+    return css;
 };
